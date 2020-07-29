fcf2e8dd80a3fbaab1f699577c122c183eec5c93
Jung Sim <tlawjd99gma@gmail.com>
2020-07-29 17:54:52 +0900

    p10818 refactored ( Scanner -> BufferedReader )

--- src/boj_java/p10818.java
+++ src/boj_java/p10818.java
@@ -1,14 +1,18 @@
 package boj_java;
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 public class p10818 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		int n, num, max=-1000001, min=1000001;
-		n = sc.nextInt();
+		n = Integer.parseInt(bf.readLine());
+		String s = bf.readLine();
+		StringTokenizer st = new StringTokenizer(s);
 		for(int i=0; i<n; i++) {
-			num = sc.nextInt();
+			num = Integer.parseInt(st.nextToken());
 			if(num > max)
 				max = num;
 			if(num < min)

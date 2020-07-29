2bc67810e1de19bec2b094243878efd8d299f0dd
Jung Sim <tlawjd99gma@gmail.com>
2020-07-29 16:31:21 +0900

    3 problems solved (class 1 essential)

---
+++ src/boj_java/p10950.java
@@ -0,0 +1,10 @@
+package boj_java;
+
+public class p10950 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+	}
+
+}

ba62a4b16b773e920614d145d086c2a12587fc60
Jung Sim <tlawjd99gma@gmail.com>
2020-09-22 14:21:06 +0900

    p2527 solved

--- src/p2527.java
+++ src/p2527.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
 
 public class p2527 {
+	static int[] lbx, lby, ltx, lty, rbx, rby, rtx, rty;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int[] lbx, lby, ltx, lty, rbx, rby, rtx, rty;
+		
 		lbx = new int[2];
 		lby = new int[2];
 		ltx = new int[2];
@@ -26,8 +27,23 @@ public class p2527 {
 				rby[i] = lby[i];
 			}
 			
-			if()
+			System.out.println(ret());
 			
 		}
+	}
+	
+	public static char ret() {
+		if(lby[0] > lty[1] || lbx[0] > rbx[1] || lby[1] > lty[0] || lbx[1] > rbx[0]) return 'd';
+		else if(chk(ltx, lty, rbx, rby) || chk(lbx, lby, rtx, rty)) return 'c';
+		else if(ltx[0] == rbx[1] || lty[0] == rby[1] || lby[0] == rty[1] || rbx[0] == lbx[1]) return 'b';
+		else return 'a';
+	}
+	
+	public static boolean chk(int[] arr1, int[] arr2, int[] arr3, int[] arr4) {
+		if(arr1[0] == arr3[1] && arr2[0] == arr4[1])
+			return true;
+		if(arr1[1] == arr3[0] && arr2[1] == arr4[0])
+			return true;
+		return false;
 	}
 }

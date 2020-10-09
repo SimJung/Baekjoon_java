3c0572fad3ca6e28fb145b2aebb4cd5e8d88f205
Jung Sim <tlawjd99gma@gmail.com>
2020-10-09 23:23:02 +0900

    p3042 solving

---
+++ src/p4485.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class p4485 {
+	public static int N;
+	public static int arr[][];
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		while(true) {
+			N = Integer.parseInt(br.readLine());
+		}
+	}
+
+}

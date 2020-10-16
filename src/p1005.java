4bf56acd3b4c93c5ce3701f11a6f6c360eed4234
Jung Sim <tlawjd99gma@gmail.com>
2020-10-16 23:57:59 +0900

    p1005 solved

--- src/p1005.java
+++ src/p1005.java
@@ -1,9 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class p1005 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static int cost[];
+	public static int toBuild[];
+	public static ArrayList<Integer> edges[];
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		
+		while(T-- > 0) {
+			int N, K;
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			K = Integer.parseInt(st.nextToken());
+			
+			cost = new int[N];
+			toBuild = new int[N];
+			edges = new ArrayList[N];
+			
+			st = new StringTokenizer(br.readLine());
+			for(int i=0; i<N; i++) {
+				cost[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			for(int i=0; i<K; i++) {
+				st = new StringTokenizer(br.readLine());
+				int x, y;
+				x = Integer.parseInt(st.nextToken());
+				y = Integer.parseInt(st.nextToken());
+				
+				
+			}
+		}
 	}
 
 }

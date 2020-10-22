426322876a10dd4ea2713f3bd29a31d186477a8b
Jung Sim <tlawjd99gma@gmail.com>
2020-10-23 03:23:45 +0900

    p17219 solved

--- src/p17219.java
+++ src/p17219.java
@@ -1,3 +1,10 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.StringTokenizer;
 
 public class p17219 {
 
@@ -5,6 +12,22 @@ public class p17219 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N, M;
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		HashMap<String, String> hm = new HashMap<>();
+		while(N-- > 0) {
+			st = new StringTokenizer(br.readLine());
+			hm.put(st.nextToken(), st.nextToken());
+		}
+		
+		while(M-- > 0) {
+			bw.write(hm.get(br.readLine())+"\n");
+		}
+		bw.flush();
+		bw.close();
 	}
 
 }

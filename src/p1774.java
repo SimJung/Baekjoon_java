b51aca4a3cb175700c4cc8920e6701706b618e98
Jung Sim <tlawjd99gma@gmail.com>
2020-10-26 23:04:53 +0900

    p9019 refactored

--- src/p1774.java
+++ src/p1774.java
@@ -2,40 +2,42 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.PriorityQueue;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class p1774 {
 	public static class Pos{
-		public int x;
-		public int y;
-		public Pos(int x, int y) {
+		public long x;
+		public long y;
+		public Pos(long x, long y) {
 			super();
 			this.x = x;
 			this.y = y;
 		}
 	}
-	public static class Info implements Comparable<Info>{
-		public int idx;
+	
+	public static class Edge implements Comparable<Edge>{
+		public int from;
+		public int to;
 		public double weight;
-		public Info(int idx, double weight) {
+		public Edge(int from, int to, double weight) {
 			super();
-			this.idx = idx;
+			this.from = from;
+			this.to = to;
 			this.weight = weight;
 		}
-		
-		public int compareTo(Info i) {
-			if(i.weight > weight) return 1;
-			else if(i.weight < weight) return -1;
+		public int compareTo(Edge e) {
+			if(e.weight > weight) return -1;
+			else if(e.weight < weight) return 1;
 			else return 0;
 		}
 	}
-	public static int N, M;
-	public static Pos arr[];
-	public static PriorityQueue<Info> edges[];
-	public static PriorityQueue<Info> pq = new PriorityQueue<>();
-	public static boolean[] visit;
+	public static int N, M, parents[], cnt=1;
+	public static Pos[] arr;
+	public static ArrayList<Edge> edges = new ArrayList<>();
 	public static double ans;
+	public static boolean[] visit;
+	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -43,29 +45,66 @@ public class p1774 {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
-		edges = new PriorityQueue[N+1];
 		arr = new Pos[N+1];
-		visit = new boolean[N+1];
+		parents = new int[N+1];
 		
 		for(int i=1; i<=N; i++) {
-			edges[i] = new PriorityQueue<>();
 			st = new StringTokenizer(br.readLine());
 			arr[i] = new Pos(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
 		}
 		
 		for(int i=1; i<N; i++) {
 			for(int j=i+1; j<=N; j++) {
-				edges[i].offer(new Info(j, getDist(i, j)));
-				edges[j].offer(new Info(i, getDist(i, j)));
+				edges.add(new Edge(i, j, getDist(i, j)));
 			}
 		}
-		
+		visit = new boolean[N+1];
+		Collections.sort(edges);
+		make();
 		for(int i=0; i<M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int from = Integer.parseInt(st.nextToken());
 			int to = Integer.parseInt(st.nextToken());
 			
+			union(from, to);
 		}
+		
+		for(int i=0; i<edges.size(); i++) {
+			if(union(edges.get(i).from, edges.get(i).to)) {
+				ans += edges.get(i).weight;
+			}
+			
+			if(cnt == N) break;
+		}
+		
+		System.out.println(String.format("%.2f", ans));
+		
+	}
+	
+	public static void make() {
+		for(int i=1; i<=N; i++) {
+			parents[i] = i;
+		}
+	}
+	
+	public static int find(int p) {
+		if(parents[p] == p) return p;
+		return parents[p] = find(parents[p]);
+	}
+	
+	public static boolean union(int a, int b) {
+		int pa = find(a);
+		int pb = find(b);
+		if(pa == pb) return false;
+		
+		if(pa > pb) {
+			parents[pa] = pb;
+		}else {
+			parents[pb] = pa;
+		}
+		cnt++;
+		return true;
+		
 	}
 	
 	public static double getDist(int i, int j) {
--- src/p9019.java
+++ src/p9019.java
@@ -4,6 +4,142 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class p9019 {
+	public static class Reg{
+		public int d[];
+		public int cur[];
+		public char c;
+		public Reg(int n, int now, char c) {
+			d = new int[4];
+			d[0] = n/1000;
+			d[1] = (n/100)%10;
+			d[2] = (n/10)%10;
+			d[3] = n%10;
+			
+			cur = new int[4];
+			cur[0] = now/1000;
+			cur[1] = (now/100)%10;
+			cur[2] = (now/10)%10;
+			cur[3] = now%10;
+			
+			this.c = c;
+		}
+		
+		public int getVal() {
+			int n = 0;
+			n += d[0] * 1000;
+			n += d[1] * 100;
+			n += d[2] * 10;
+			n += d[3];
+			return n;
+		}
+		
+		public int getNow() {
+			int n = 0;
+			n += cur[0] * 1000;
+			n += cur[1] * 100;
+			n += cur[2] * 10;
+			n += cur[3];
+			return n;
+		}
+		
+		public int getD() {
+			int n = (getNow()<<1)%10000;
+			return n;
+		}
+		
+		public int getS() {
+			int n = getNow();
+			return (n == 0 ? 9999 : n-1);
+		}
+		
+		public int getL() {
+			int n = 0;
+			n += cur[1] * 1000;
+			n += cur[2] * 100;
+			n += cur[3] * 10;
+			n += cur[0];
+			return n;
+		}
+		
+		public int getR() {
+			int n = 0;
+			n += cur[3] * 1000;
+			n += cur[0] * 100;
+			n += cur[1] * 10;
+			n += cur[2];
+			return n;
+		}
+	}
+	
+	public static Reg[] visit = new Reg[10000];
+	public static Queue<Reg> q = new LinkedList<>();
+	
+	public static int N;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		
+		StringBuilder sb = new StringBuilder();
+		while(N-- > 0) {
+			int from = sc.nextInt();
+			int to = sc.nextInt();
+			q.clear();
+			Arrays.fill(visit, null);
+			visit[from] = new Reg(from, from, ' ');
+			q.offer(visit[from]);
+			sb.delete(0, sb.length());
+			
+			while(!q.isEmpty()) {
+				Reg reg = q.poll();
+				
+				int newNum = reg.getD();
+				if(visit[newNum] == null) {
+					visit[newNum] = new Reg(reg.getNow(), newNum, 'D');
+					q.offer(visit[newNum]);
+				}
+				
+				newNum = reg.getS();
+				if(visit[newNum] == null) {
+					visit[newNum] = new Reg(reg.getNow(), newNum, 'S');
+					q.offer(visit[newNum]);
+				}
+				
+				newNum = reg.getL();
+				if(visit[newNum] == null) {
+					visit[newNum] = new Reg(reg.getNow(), newNum, 'L');
+					q.offer(visit[newNum]);
+				}
+				
+				newNum = reg.getR();
+				if(visit[newNum] == null) {
+					visit[newNum] = new Reg(reg.getNow(), newNum, 'R');
+					q.offer(visit[newNum]);
+				}
+				
+				if(visit[to] != null) break;
+			}
+			
+			while(true) {
+				if(to == from) break;
+				sb.insert(0, visit[to].c);
+				to = visit[to].getVal();
+			}
+			
+			System.out.println(sb.toString());
+			
+		}
+	}
+}
+
+/*
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class p9019 {
+	
 	public static class Info{
 		public int num;
 		public int prevNum;
@@ -88,5 +224,6 @@ public class p9019 {
 			System.out.println(answer.toString());
 		}
 	}
-
+	
 }
+*/

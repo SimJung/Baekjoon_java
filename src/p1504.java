12eba787e82b7dd4b6567ca5380373195437d93f
Jung Sim <tlawjd99gma@gmail.com>
2020-11-18 18:01:42 +0900

    p1504 solving

---
+++ src/p1504.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class p1504 {
+	public static class Edge{
+		public int to;
+		public int weight;
+		
+	}
+	public static int N, E;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		E = Integer.parseInt(st.nextToken());
+		
+		for(int i=0; i<E; i++) {
+			st = new StringTokenizer(br.readLine());
+			int from = I
+		}
+	}
+
+}

34a9c8a47ca92abecf703cd1c91e4fd3cf1aece0
Jung Sim <tlawjd99gma@gmail.com>
2020-11-25 23:03:20 +0900

    p2206 solved

--- src/p2206.java
+++ src/p2206.java
@@ -1,78 +1,97 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.StringTokenizer;
-import java.util.Queue;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 public class p2206 {
-	static int N, M;
-	static boolean arr[][];
-	static Queue<int[]> q= new LinkedList<>();
-	static int dir[][] = {
+	public static class Info{
+		public int i;
+		public int j;
+		public int cnt;
+		public int wallBreak;
+		public Info(int i, int j, int cnt, int wallBreak) {
+			super();
+			this.i = i;
+			this.j = j;
+			this.cnt = cnt;
+			this.wallBreak = wallBreak;
+		}
+		
+	}
+	public static int N, M;
+	public static char arr[][];
+	public static int visit[][][];
+	public static Queue<Info> q = new LinkedList<>();
+	public static int delta[][] = {
 			{0, -1},
 			{0, 1},
 			{-1, 0},
-			{1, 0}
+			{1, 0},
 	};
-	
-	static class Count{
-		public int cnt = 0;
-		public boolean wallBreak = false;
-	}
-	
-	static Count[][] arrCount;
 	public static void main(String[] args) throws IOException{
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(bf.readLine());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
-		arr = new boolean[N][M];
-		arrCount = new Count[N][M];
+		visit = new int[N][M][2];
+		arr = new char[N][];
 		for(int i=0; i<N; i++) {
+			arr[i] = br.readLine().toCharArray();
+			
 			for(int j=0; j<M; j++) {
-				arrCount[i][j] = new Count();
+				visit[i][j][0] = Integer.MAX_VALUE;
+				visit[i][j][1] = Integer.MAX_VALUE;
 			}
 		}
 		
-		String temp;
-		for(int i=0; i<N; i++) {
-			temp = bf.readLine();
-			for(int j=0; j<M; j++) {
-				arr[i][j] = (temp.charAt(j) == '1' ? true : false);
-				arrCount[i][j].cnt = -1;
+		visit[0][0][0] = 1;
+		q.offer(new Info(0, 0, 1, 0));
+		
+		while(!q.isEmpty()) {
+			Info now = q.poll();
+			if(now.i == N-1 && now.j == M-1) {
+				//show();
+				System.out.println(now.cnt);
+				return ;
+			}
+			
+			for(int d=0; d<4; d++) {
+				int ni = now.i + delta[d][0];
+				int nj = now.j + delta[d][1];
+				
+				if(ni < 0 || ni >= N || nj < 0 || nj >= M) continue;
+				
+				if(arr[ni][nj] == '1') {
+					if(now.wallBreak == 1 || visit[ni][nj][1] <= now.cnt+1) continue;	
+				}else {
+					if(visit[ni][nj][now.wallBreak] <= now.cnt+1) continue;
+				}
+				
+				
+				if(arr[ni][nj] == '1') {
+					visit[ni][nj][1] = now.cnt+1;
+					q.offer(new Info(ni, nj, now.cnt+1, 1));
+				}else {
+					visit[ni][nj][now.wallBreak] = now.cnt+1;
+					q.offer(new Info(ni, nj, now.cnt+1, now.wallBreak));
+				}
 			}
 		}
 		
-		arrCount[0][0].cnt = 0;
-		q.offer(new int[] {0, 0});
-		bfs();
-		System.out.println(arrCount[N-1][M-1].cnt);
+		//show();
+		
+		
+		System.out.println("-1");
 	}
 	
-	public static void bfs() {
-		while(!q.isEmpty()) {
-			int i = q.peek()[0];
-			int j = q.peek()[1];
-			q.poll();
-			
-			for(int k=0; k<4; k++) {
-				int ni = i+dir[k][0];
-				int nj = j+dir[k][1];
-				if(ni >= N || ni <0 || nj >= M || nj < 0)
-					continue;
-				
-				if(arrCount[i][j].wallBreak && arr[ni][nj])
-					continue;
-				
-				if(arrCount[ni][nj].cnt != -1 && arrCount[i][j].cnt+1 >= arrCount[ni][nj].cnt)
-					continue;
-				
-				arrCount[ni][nj].cnt = arrCount[i][j].cnt+1;
-				if(arr[ni][nj])	arrCount[ni][nj].wallBreak = true;
-				q.offer(new int[] {ni, nj});
+	public static void show() {
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<M; j++) {
+				System.out.print("("+visit[i][j][0]+","+visit[i][j][1]+") ");
 			}
+			System.out.println();
 		}
 	}
-
 }

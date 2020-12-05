3e785be0ca0775af5b34065e6040d7999cffe78d
Jung Sim <tlawjd99gma@gmail.com>
2020-12-05 23:22:33 +0900

    p2420 solved

---
+++ src/p2420.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class p2420 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		long n, m;
+		n = sc.nextInt();
+		m = sc.nextInt();
+		
+		System.out.println((long)(Math.abs(n - m)));
+	}
+
+}

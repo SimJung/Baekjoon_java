58bf62c82f5dfd2223e98acfdce13f2811f40d46
Jung Sim <tlawjd99gma@gmail.com>
2020-12-14 23:55:43 +0900

    p1918 solved

--- src/p1918.java
+++ src/p1918.java
@@ -3,25 +3,97 @@ import java.util.Scanner;
 import java.util.Stack;
 public class p1918 {
 	
-	public static String origin;
-	public static StringBuilder sb = new StringBuilder();
+	public static StringBuilder origin, result = new StringBuilder();
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		origin = sc.next();
+		origin = new StringBuilder(sc.next());
+		
+		for(int i=0; i<origin.length(); i++) {
+			if(origin.charAt(i) == '*' || origin.charAt(i) == '/') {
+				if(origin.charAt(i-1) == ')') {
+					int bn = 1, idx = i;
+					for(int j=i-2; j>=0; j--) {
+						if(origin.charAt(j) == '(')
+							bn--;
+						else if(origin.charAt(j) == ')')
+							bn++;
+						
+						if(bn == 0) {
+							idx = j;
+							break;
+						}
+					}
+					
+					origin.insert(idx, '(');
+				}else {
+					origin.insert(i-1, '(');
+				}
+				
+				i++;
+				
+				
+				
+				if(origin.charAt(i+1) == '(') {
+					int bn = 1, idx = i;
+					for(int j=i+2; j<origin.length(); j++) {
+						if(origin.charAt(j) == ')')
+							bn--;
+						else if(origin.charAt(j) == '(')
+							bn++;
+						
+						if(bn == 0) {
+							idx = j;
+							break;
+						}
+					}
+					
+					origin.insert(idx, ')');
+				}else {
+					origin.insert(i+2, ')');
+				}
+				
+			}
+		}
+		
+		System.out.println(origin.toString());
 
-		sb.append(makeStr(0));
+		Stack<Character> st = new Stack<>();
+		boolean isOk = false;
+		for(int i=0; i<origin.length(); i++) {
+			switch(origin.charAt(i)) {
+			case '+':
+			case '*':
+			case '-':
+			case '/':
+				st.push(origin.charAt(i));
+				if(origin.charAt(i+1) != '(') isOk = true;
+				break;
+				
+			case '(':
+				break;
+				
+			case ')':
+				if(!st.isEmpty()) {
+					result.append(st.pop());
+					isOk = false;
+				}
+				break;
+				
+			default:
+				result.append(origin.charAt(i));
+				if(isOk) {
+					if(!st.isEmpty()) {
+						result.append(st.pop());
+						isOk = false;
+					}
+				}
+				break;
+				
+			}
+		}
+		
+		System.out.println(result.toString());
 	}
 	
-	public static String makeStr(int start) {
-		StringBuilder temp = new StringBuilder();
-		
-		temp.append("(");
-		
-		temp.append(makeStr(start));
-		
-		temp.append(")");
-		
-		return temp.toString();
-	}
 }
